e2cad7c2ebf6f4fc6a2319315c25cbc919191524
Anup29 <anup03@hotmail.com>
2022-07-07 10:54:33 +0530

    Update and rename getEnv_api.groovy to getEnvApi.groovy

--- vars/getEnvApi.groovy
+++ vars/getEnvApi.groovy
@@ -1,4 +1,4 @@
-def getEnv_api(String env) {
+def getEnvApi(String env) {
     if(env.contains("stg")) {return "stg.skyscraper.vmware.com";
     } else if(env.contains("dev")) {return "dev.skyscraper.vmware.com";
     } else { return "PROD"; }
