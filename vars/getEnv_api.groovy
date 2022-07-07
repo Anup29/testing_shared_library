8b2fd5bfc034778a4cb14fe69c4ce460045bd35f
Anup29 <anup03@hotmail.com>
2022-07-07 10:53:27 +0530

    Create getEnv_api.groovy

---
+++ vars/getEnv_api.groovy
@@ -0,0 +1,5 @@
+def getEnv_api(String env) {
+    if(env.contains("stg")) {return "stg.skyscraper.vmware.com";
+    } else if(env.contains("dev")) {return "dev.skyscraper.vmware.com";
+    } else { return "PROD"; }
+}
