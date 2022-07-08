fc796443a332adae13f20fbf3875e226fd6da595
Anup29 <anup03@hotmail.com>
2022-07-08 09:51:00 +0530

    Update calc.groovy

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -5,3 +5,11 @@ def add(x,y){
 def sub(x,y){
   echo "Diff of ${x} and ${y} : ${x-y}"
 }
+
+def div(x,y){
+  catchError(buildResult: 'UNSTABLE', stageResult: 'FAILURE') {
+        script{
+          sh ${x}/${y}
+        }
+    archiveArtifacts artifacts: 'racetrack_link.txt'
+  }
