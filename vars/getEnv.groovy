75b0b5e92eb79ce39f49b4f25b2bc75c0807719c
Anup29 <anup03@hotmail.com>
2022-07-19 16:18:52 +0530

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -1,4 +1,8 @@
+import vars.calc;
+
 def call(String env) {
+    def r = calc.sub(10,5)
+    echo "${r}"
     if(env.contains("stg")) { return "STG";
     } else if(env.contains("dev")) { return "DEV";
     } else { return "PROD"; }
