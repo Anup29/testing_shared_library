439fcbbc290271e917b700d408c45a57ffb7f8db
Anup29 <anup03@hotmail.com>
2022-07-19 16:27:12 +0530

    Update getEnv.groovy

--- vars/getEnv.groovy
+++ vars/getEnv.groovy
@@ -2,7 +2,6 @@ import static calc.*
 
 def call(String env) {
     def r = calc.sub(10,5)
-    echo "${r}"
     if(env.contains("stg")) { return "STG";
     } else if(env.contains("dev")) { return "DEV";
     } else { return "PROD"; }
