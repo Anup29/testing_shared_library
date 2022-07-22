3e121df033b82827c825f0f85fa9235915ec1cfb
Anup29 <anup03@hotmail.com>
2022-07-22 18:14:20 +0530

    Update map_param.groovy

--- vars/map_param.groovy
+++ vars/map_param.groovy
@@ -2,7 +2,7 @@ def call(Map config){
   def st = ""
   for ( ob in config) {
     println "Key= ${ob.key}, value= ${ob.value}"
-    st = st + '--pytest-args="--${ob.key}=${ob.value}"'
+    st = st + '--pytest-args="--' +${ob.key} + '=' + ${ob.value} + '"'
   }
   println(st)
 }
